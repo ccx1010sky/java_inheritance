f94222c1bfc43d17cd97cfe6a375c48ce8a9ebe2
ccx1010sky <ccx1010@gmail.com>
2022-02-09 10:40:05 +0000

    test Person.java pass

---
+++ src/main/java/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+    String name;
+    String cohort;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCohort(String cohort) {
+        this.cohort = cohort;
+    }
+
+    public Person(String name, String cohort){
+        this.name = name;
+        this.cohort = cohort;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCohort() {
+        return cohort;
+    }
+    public String talk(String language){
+        return "I love " + language;
+    }
+
+
+
+
+}
