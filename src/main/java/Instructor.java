afd19e36d0154bf3c975a3ded50775585e1240eb
ccx1010sky <ccx1010@gmail.com>
2022-02-09 11:11:02 +0000

    15 test

--- src/main/java/Instructor.java
+++ src/main/java/Instructor.java
@@ -1,30 +1,42 @@
-public class Instructor {
+public class Instructor extends Person {
 
-    private String name;
-    private String cohort;
+//    private String name;
+//    private String cohort;
 
-    public Instructor(String name, String cohort) {
-        this.name = name;
-        this.cohort = cohort;
+    private String moduleTeam;
+
+    public Instructor(String name, String cohort, String moduleTeam) {
+        super(name,cohort);
+        this.moduleTeam = moduleTeam;
     }
 
-    public String getName() {
-        return name;
+    public String getModuleTeam() {
+        return moduleTeam;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setModuleTeam(String moduleTeam) {
+        this.moduleTeam = moduleTeam;
     }
 
-    public String getCohort() {
-        return cohort;
-    }
 
-    public void setCohort(String cohort) {
-        this.cohort = cohort;
-    }
 
-    public String talk(String language){
-        return "I love " + language;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getCohort() {
+//        return cohort;
+//    }
+//
+//    public void setCohort(String cohort) {
+//        this.cohort = cohort;
+//    }
+//
+//    public String talk(String language){
+//        return "I love " + language;
+//    }
 }
--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,29 +1,13 @@
-public class Student {
+public class Student extends Person{
     private String name;
     private String cohort;
 
     public Student(String name, String cohort) {
-        this.name = name;
-        this.cohort = cohort;
+       super(name,cohort);
     }
 
-    public String getName() {
-        return name;
+    public  String talk(String language){
+        return "I love learning " + language;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCohort() {
-        return cohort;
-    }
-
-    public void setCohort(String cohort) {
-        this.cohort = cohort;
-    }
-
-    public String talk(String language){
-        return "I love " + language;
-    }
 }
